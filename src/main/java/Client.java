35e4d1fa96a7cfd8082e1d9bb586084f2618dd03
Daria Suntcova <batareyka23@gmail.com>
2022-11-27 14:27:13 +0300

    add Client-server with ruffles

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,16 +6,22 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        String host = "127.0.0.1";
+        String host = "netology.homework";
         int port = 8089;
 
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
         ){
+            System.out.println(in.readLine());
             out.println("Daria");
-            String resp = in.readLine();
-            System.out.println(resp);
+
+            System.out.println(in.readLine());
+//            out.println("yes");
+            out.println("no");
+
+            System.out.println(in.readLine());
+
 
         } catch (IOException e){
             e.printStackTrace();
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -15,12 +15,21 @@ public class Server {
                 try (Socket clientSocket = serverSocket.accept();
                      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-                ){
-                    System.out.printf("New connection accepted. Port: %d%n",clientSocket.getPort() );
+                ) {
+                    System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
 
+                    out.println("Hello! Write your name");
                     final String name = in.readLine();
 
-                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                    out.println("Are you child? (yes/no)");
+
+                    String childOrNot = in.readLine();
+                    if (childOrNot.equals("yes")) {
+                        out.printf("Welcome to the kids area, %s! Let's play!", name);
+                    } else {
+                        out.printf("Welcome to the adult zone, %s! Have a good rest, or a good working day!"
+                                , name);
+                    }
                 }
             }
         }
